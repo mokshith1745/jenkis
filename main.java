7605dd56a9357236d52d87d84f88189c640dc31a
mokshith1745 <ketharimokshithreddy@gmail.com>
2025-10-29 16:26:48 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
-public class Main{
-    public static void main(String [] args){
-        System.out.println("HELLO ");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
+}
